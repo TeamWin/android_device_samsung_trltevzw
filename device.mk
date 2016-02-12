3e7eb4bab5d744dd83e4638cf09a3d84c59a3dec
Hamada Badr <hamada.s.badr@gmail.com>
2016-02-12 16:58:25 -0500

    trltevzw: Fixes and updates for TWRP 3 (android-6.0 branch)
    * Update TWRP and board configs
    * Update system properties
    * Use custom bootimg.mk
    * Add dtbtool source
    * Remove prebuilt kernel
    
    Change-Id: I43c5cf63b0f48e7bffcaadc85e0382469ada59b9

--- device.mk
+++ device.mk
@@ -3,19 +3,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
-$(call inherit-product-if-exists, vendor/samsung/trltevzw/trltetmo-vendor.mk)
+$(call inherit-product-if-exists, vendor/samsung/trltevzw/trltevzw-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/samsung/trltevzw/overlay
 
 LOCAL_PATH := device/samsung/trltevzw
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := $(LOCAL_PATH)/kernAl
-else
-	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
+PRODUCT_PACKAGES += dtbToolTW
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/fstab.qcom:recovery/root/fstab.qcom
